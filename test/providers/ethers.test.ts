8bfe34d2f8be3a37d8493320013d16036e4ff7c8
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2019-12-19 13:48:35 +0100

    Refactor to a more functional approach

--- test/providers/ethers.test.ts
+++ test/providers/ethers.test.ts
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
 import { JsonRpcProvider } from '@ethersproject/providers';
-import { EthersProvider } from '../../src/providers';
-import { decode, encodeWithId, stringToBuffer } from '../../src/utils';
+import { callWithEthers } from '../../src';
+import { decode, encodeWithId } from '../../src/utils';
 import {
   ETHER_BALANCES_ID,
   ETHER_BALANCES_TYPE,
@@ -13,21 +13,21 @@ import BigNumber from 'bignumber.js';
 const BalanceScanner = artifacts.require('BalanceScanner');
 const FixedBalanceToken = artifacts.require('FixedBalanceToken');
 
-const LOCAL_PROVIDER = web3.currentProvider.host;
+const LOCAL_PROVIDER: string = (web3 as any).currentProvider.host;
 
 describe('providers/ethers', () => {
-  describe('EthersProvider', async () => {
-    const provider = new EthersProvider(new JsonRpcProvider(LOCAL_PROVIDER));
+  describe('callWithEthers()', async () => {
+    const provider = new JsonRpcProvider(LOCAL_PROVIDER);
 
     it('should get Ether balances from the contract', async () => {
-      const contract = await BalanceScanner.deployed();
+      const { address } = await BalanceScanner.deployed();
       const accounts = await web3.eth.getAccounts();
       accounts.shift();
 
       const data = encodeWithId(ETHER_BALANCES_ID, ETHER_BALANCES_TYPE, accounts);
-      const response = await provider.call(contract.address, data);
+      const response = await callWithEthers(provider, address, data);
 
-      const decoded = decode(stringToBuffer(response));
+      const decoded = decode(response);
 
       for (let i = 0; i < accounts.length; i++) {
         const balance = new BigNumber(await web3.eth.getBalance(accounts[i]));
@@ -36,14 +36,14 @@ describe('providers/ethers', () => {
     });
 
     it('should get token balances from the contract', async () => {
-      const contract = await BalanceScanner.deployed();
+      const { address } = await BalanceScanner.deployed();
       const token = await FixedBalanceToken.new();
       const accounts = await web3.eth.getAccounts();
 
       const data = encodeWithId(TOKEN_BALANCES_ID, TOKEN_BALANCES_TYPE, accounts, token.address);
-      const response = await provider.call(contract.address, data);
+      const response = await callWithEthers(provider, address, data);
 
-      const decoded = decode(stringToBuffer(response));
+      const decoded = decode(response);
 
       for (let i = 0; i < accounts.length; i++) {
         expect(decoded[i].isEqualTo(new BigNumber('100000000000000000000'))).to.equal(true);
