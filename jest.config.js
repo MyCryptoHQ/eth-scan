4ba86f6902a52809228aa4bbefafcb4a338b6d7b
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2021-01-04 16:10:01 +0100

    Replace tokens balances call with per address calls

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   roots: ['src/', 'tests/'],
   clearMocks: true,
-  collectCoverage: true,
   collectCoverageFrom: ['**/*.ts?(x)', '!**/*.d.ts', '!src/contracts/**/*', '!src/vendor/**/*'],
   transform: {
     '^.+\\.[t|j]sx?$': 'babel-jest'
