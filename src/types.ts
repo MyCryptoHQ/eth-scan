254718f2e1dce125f0ce6d5b771f03d6cb69f8f6
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2021-01-09 15:29:01 +0100

    Refactor to reduce code duplication (#207)

---
+++ src/types.ts
@@ -0,0 +1,19 @@
+export interface EthScanOptions {
+  /**
+   * The address of the contract to use. Defaults to 0xbb4AAaF8cAA1A575B43E7673e5b155C1c5A8BC13.
+   */
+  contractAddress?: string;
+
+  /**
+   * It's not possible to check thousands of addresses at the same time, due to gas limitations.
+   * Calls are split per `batchSize` addresses, by default set to 1000.
+   */
+  batchSize?: number;
+}
+
+/**
+ * An object that contains the address (key) and balance or balance (value).
+ */
+export interface BalanceMap<T = bigint> {
+  [key: string]: T;
+}
