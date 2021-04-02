f9fbe22cbb56a6acc0e2901362e74d33ff2f1eb3
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2021-04-02 13:50:27 +0200

    Remove unused function

--- contracts/BalanceScanner.sol
+++ contracts/BalanceScanner.sol
@@ -42,25 +42,6 @@ contract BalanceScanner {
   }
 
   /**
-   * @notice Get the ERC-20 token balances for multiple contracts, for multiple addresses
-   * @dev This does not check if the `token` address specified is actually an ERC-20 token
-   * @param addresses The addresses to get the token balances for
-   * @param contracts The addresses of the ERC-20 token contracts
-   * @return balances The token balances in the same order as the addresses specified
-   */
-  function tokensBalances(address[] calldata addresses, address[] calldata contracts)
-    external
-    view
-    returns (uint256[][] memory balances)
-  {
-    balances = new uint256[][](addresses.length);
-
-    for (uint256 i = 0; i < addresses.length; i++) {
-      balances[i] = tokensBalance(addresses[i], contracts);
-    }
-  }
-
-  /**
    * @notice Get the ERC-20 token balance from multiple contracts for a single owner
    * @param owner The address of the token owner
    * @param contracts The addresses of the ERC-20 token contracts
